405ffc3181d29bdc78b45076134948def361f164
Eduardo Eidelwein Berlitz <eberlitz@gmail.com>
2020-03-07 13:01:20 -0300

    Update shell_utils.sh

--- shell_utils.sh
+++ shell_utils.sh
@@ -1,1 +1,7 @@
-alias py2_create_env="python2 -m virtualenv -p /usr/bin/python2.7 ~/.virtualens/$1"
+py2_create_env(){
+  python2.7 -m virtualenv -p /usr/bin/python2.7 ~/.virtualens/$1
+}
+
+py_workon(){
+  source ~/.virtualens/$1/bin/activate
+}
